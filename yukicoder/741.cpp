d168ed0069c3a808c9fc16a91d186ed6faaae3c5
mint <distout.215@gmail.com>
2019-07-19 00:19:46 +0900

    add new file

---
+++ yukicoder/741.cpp
@@ -0,0 +1,60 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+#define inf 1000000000
+#define INF 1000000000000000
+#define ll long long
+#define ull unsigned long long
+#define M (int)(1e9+7)
+#define P pair<int,int>
+#define PLL pair<ll,ll>
+#define FOR(i,m,n) for(int i=(int)m;i<(int)n;i++)
+#define RFOR(i,m,n) for(int i=(int)m;i>=(int)n;i--)
+#define rep(i,n) FOR(i,0,n)
+#define rrep(i,n) RFOR(i,n,0)
+#define all(a) a.begin(),a.end()
+#define IN(a,n) rep(i,n){ cin>>a[i]; }
+const int vx[4] = {0,1,0,-1};
+const int vy[4] = {1,0,-1,0};
+#define PI 3.14159265
+#define F first
+#define S second
+#define PB push_back
+#define EB emplace_back
+#define int ll
+#define vi vector<int>
+
+
+
+int fac[3100000];
+
+int inv(int x){
+  if(x==1) return 1;
+  return (M-M/x)*inv(M%x)%M;
+}
+
+
+int comb(int n,int k){
+  int x=(fac[k]*fac[n-k])%M;
+  return (fac[n]*inv(x))%M;
+}
+
+void fac_init(int n){
+  fac[0]=1;
+  FOR(i,1,n+1){
+    fac[i]=fac[i-1]*i;
+    fac[i]%=M;
+  }
+}
+
+main(){
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+
+  int n;
+  cin>>n;
+  fac_init(n+100);
+
+  cout<<comb(n+10-1,n)<<endl;
+
+}
