9ac9aa06c525f1697c89dd55bec1aec5cb0c4e91
master <distout.215@gmail.com>
2019-07-29 22:20:57 +0900

    add new file

---
+++ atcoder/abc/135/abc_135_d.cpp
@@ -0,0 +1,54 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+#define inf INT_MAX
+#define INF 1000000000000000
+#define ll long long
+#define ull unsigned long long
+#define M (int)(1e9+7)
+#define P pair<int,int>
+#define PLL pair<ll,ll>
+#define FOR(i,m,n) for(int i=(int)m;i<(int)n;i++)
+#define RFOR(i,m,n) for(int i=(int)m;i>=(int)n;i--)
+#define rep(i,n) FOR(i,0,n)
+#define rrep(i,n) RFOR(i,n,0)
+#define all(a) a.begin(),a.end()
+#define IN(a,n) rep(i,n){ cin>>a[i]; }
+const int vx[4] = {0,1,0,-1};
+const int vy[4] = {1,0,-1,0};
+#define PI 3.14159265
+#define F first
+#define S second
+#define PB push_back
+#define EB emplace_back
+#define int ll
+#define vi vector<int>
+
+
+int dp[200000][130];
+
+main(){
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+
+  string s;
+  cin>>s;
+
+  dp[0][0]=1;
+  rep(i,s.size()){
+    int c;
+    if(s[i]=='?') c=10;
+    else c=s[i]-'0';
+
+    rep(j,10){
+     if(c!=10&&c!=j) continue; 
+     rep(k,13){
+       dp[i+1][(k*10+j)%13]+=dp[i][k];
+       dp[i+1][(k*10+j)%13]%=M;
+     }
+    }
+  }
+
+  cout<<dp[s.size()][5]<<endl;
+
+}
