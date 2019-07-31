d9068cccb73727223ff78264b8ef118ae1abece9
master <distout.215@gmail.com>
2019-07-31 20:50:33 +0900

    add new file

---
+++ codeforces/1197/1197B.cpp
@@ -0,0 +1,55 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+#define inf INT_MAX
+#define INF LLONG_MAX
+#define ll long long
+#define ull unsigned long long
+#define M (int)(1e9+7)
+#define P pair<int,int>
+#define PLL pair<ll,ll>
+#define FOR(i,m,n) for(int i=(int)m;i<(int)n;i++)
+#define RFOR(i,m,n) for(int i=(int)m;i>=(int)n;i--)
+#define rep(i,n) FOR(i,0,n)
+#define rrep(i,n) RFOR(i,n,0)
+#define all(a) a.begin(),a.end()
+#define IN(a,n) rep(i,n){ cin>>a[i]; }
+const int vx[4] = {0,1,0,-1};
+const int vy[4] = {1,0,-1,0};
+#define PI 3.14159265
+#define F first
+#define S second
+#define PB push_back
+#define EB emplace_back
+#define int ll
+#define vi vector<int>
+
+
+
+signed main(){
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+
+  int n;
+  cin>>n;
+  vector<int> a(n);
+  rep(i,n){
+    cin>>a[i];
+  }
+
+  int p=max_element(all(a))-a.begin();
+
+  bool flag=true;
+  rep(i,p){
+    flag&=(a[i]<a[i+1]);
+  }
+  FOR(i,p,n-1){
+    flag&=(a[i]>a[i+1]);
+  }
+
+  if(flag)
+    cout<<"YES"<<endl;
+  else
+    cout<<"NO"<<endl;
+
+}
