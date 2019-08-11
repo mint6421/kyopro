d9e2af06dd1731b83310f4b4d55a29f811ec8c92
master <distout.215@gmail.com>
2019-08-11 23:58:27 +0900

    add new file

---
+++ yukicoder/183.cpp
@@ -0,0 +1,51 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+#define inf INT_MAX
+#define INF LLONG_MAX
+#define ll long long
+#define ull unsigned long long
+#define M (int)(1e9+7)
+#define P pair<int,int>
+#define PLL pair<ll,ll>
+#define FOR(i,m,n) for(int i=(int)m;i<(int)n;i++)
+#define RFOR(i,m,n) for(int i=(int)m;i>=(int)n;i--)
+#define rep(i,n) FOR(i,0,n)
+#define rrep(i,n) RFOR(i,n,0)
+#define all(a) a.begin(),a.end()
+#define IN(a,n) rep(i,n){ cin>>a[i]; }
+const int vx[4] = {0,1,0,-1};
+const int vy[4] = {1,0,-1,0};
+#define PI 3.14159265
+#define F first
+#define S second
+#define PB push_back
+#define EB emplace_back
+#define int ll
+#define vi vector<int>
+
+
+
+signed main(){
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+
+  int n;
+  cin>>n;
+  vi a(n);
+  rep(i,n){
+    cin>>a[i];
+  }
+  sort(all(a));
+  bitset<(1<<15)> bit;
+  bit[0]=true;
+  rep(i,n){
+    rep(j,(1<<15)){
+      bit[a[i]^j]=(bit[a[i]^j]|bit[j]);
+    }
+  }
+
+  cout<<bit.count()<<endl;
+
+}
+
