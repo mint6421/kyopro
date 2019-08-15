d4e638a9fc4995cc770f473ba0804edc6fd48ee7
master <distout.215@gmail.com>
2019-08-15 21:42:47 +0900

    add new file

---
+++ yukicoder/312.cpp
@@ -0,0 +1,49 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+#define inf INT_MAX
+#define INF LLONG_MAX
+#define ll long long
+#define ull unsigned long long
+#define M (int)(1e9+7)
+#define P pair<int,int>
+#define PLL pair<ll,ll>
+#define FOR(i,m,n) for(int i=(int)m;i<(int)n;i++)
+#define RFOR(i,m,n) for(int i=(int)m;i>=(int)n;i--)
+#define rep(i,n) FOR(i,0,n)
+#define rrep(i,n) RFOR(i,n,0)
+#define all(a) a.begin(),a.end()
+#define IN(a,n) rep(i,n){ cin>>a[i]; }
+const int vx[4] = {0,1,0,-1};
+const int vy[4] = {1,0,-1,0};
+#define PI 3.14159265
+#define F first
+#define S second
+#define PB push_back
+#define EB emplace_back
+#define int ll
+#define vi vector<int>
+
+
+
+signed main(){
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+
+  int n;
+  cin>>n;
+  int ans=n;
+  FOR(i,2,1e6+1){
+    if(n%i==0){
+      if(i==2){
+        if(n/2==2) ans=n;
+        else ans=n/2;
+      }
+      else ans=min(ans,i);
+    }
+  }
+
+  cout<<ans<<endl;
+
+
+}
