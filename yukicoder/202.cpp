b30a4b5913af77c6b6410ecb01e3b9d9a6a24c97
master <distout.215@gmail.com>
2019-08-19 22:14:52 +0900

    add new file

---
+++ yukicoder/202.cpp
@@ -0,0 +1,61 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+#define inf INT_MAX
+#define INF LLONG_MAX
+#define ll long long
+#define ull unsigned long long
+#define M (int)(1e9+7)
+#define P pair<int,int>
+#define PLL pair<ll,ll>
+#define FOR(i,m,n) for(int i=(int)m;i<(int)n;i++)
+#define RFOR(i,m,n) for(int i=(int)m;i>=(int)n;i--)
+#define rep(i,n) FOR(i,0,n)
+#define rrep(i,n) RFOR(i,n,0)
+#define all(a) a.begin(),a.end()
+#define IN(a,n) rep(i,n){ cin>>a[i]; }
+const int vx[4] = {0,1,0,-1};
+const int vy[4] = {1,0,-1,0};
+#define PI 3.14159265
+#define F first
+#define S second
+#define PB push_back
+#define EB emplace_back
+#define int ll
+#define vi vector<int>
+
+
+
+signed main(){
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+
+  int n;
+  cin>>n;
+  vector<P> v(n);
+  rep(i,n){
+    cin>>v[i].F>>v[i].S;
+    v[i].F+=20;
+  }
+
+  set<int> d[30000];
+  int ans=0;
+
+  rep(i,n){
+    bool flag=true;
+    FOR(x,-19,20){
+      FOR(y,-19,20){
+        if(x*x+y*y>=400) continue;
+        flag&=(d[v[i].F+x].find(v[i].S+y)==d[v[i].F+x].end());
+      }
+    }
+    if(flag){
+      d[v[i].F].insert(v[i].S);
+      ans++;
+    }
+  }
+
+
+  cout<<ans<<endl;
+
+}
