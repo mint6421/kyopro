c2a64dc76a63480e645905b78f92b532dc42b802
master <distout.215@gmail.com>
2019-09-24 22:46:12 +0900

    2019/09/24

--- shellscript/auto_commit.sh
+++ shellscript/auto_commit.sh
@@ -4,6 +4,19 @@ while true
 do
   git add *
   git commit -m `date "+%Y/%m/%d"`
+
+  Username=$(openssl rsautl -decrypt -inkey ~/.ssh/id_rsa -in ~/key/user.rsa)
+  Password=$(openssl rsautl -decrypt -inkey ~/.ssh/id_rsa -in ~/key/pass.rsa)
+
+  expect -c "
+  spawn git push
+  expect Username\ ; send $Username; send \r
+  expect Password\ ; send $Password; send \r;
+  expect eof
+  "
+
+
   sleep 300
+
 done
 
