7c36590a3e42aa93df9d345f29b6ce608b3244d5
ee177100 <ee177100@meiji.ac.jp>
2019-11-25 22:15:10 +0900

    2019/11/25

---
+++ atcoder/Other/relay2/relay2_b.cpp
@@ -0,0 +1,62 @@
+
+
+#include<bits/stdc++.h>
+using namespace std;
+#define inf INT_MAX
+#define INF LLONG_MAX
+#define ll long long
+#define ull unsigned long long
+#define M (int)(1e9+7)
+#define P pair<int,int>
+#define FOR(i,m,n) for(int i=(int)m;i<(int)n;i++)
+#define RFOR(i,m,n) for(int i=(int)m;i>=(int)n;i--)
+#define rep(i,n) FOR(i,0,n)
+#define rrep(i,n) RFOR(i,n,0)
+#define all(a) a.begin(),a.end()
+const int vx[4] = {0,1,0,-1};
+const int vy[4] = {1,0,-1,0};
+#define F first
+#define S second
+#define PB push_back
+#define EB emplace_back
+#define int ll
+#define vi vector<int>
+#define IP pair<int,P>
+#define PI pair<P,int>
+#define PP pair<P,P>
+#define Yes(f){cout<<(f?"Yes":"No")<<endl;}
+#define YES(f){cout<<(f?"YES":"NO")<<endl;}
+#define MPlus(x,y) {x=(x+y)%M;}
+#define MSub(x,y) {x=(x-y+M)%M;}
+#define MMul(x,y) {x=(x*y)%M;}
+
+
+signed main(){
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  cout<<fixed<<setprecision(20);
+
+  int n,q;
+  cin>>n>>q;
+  while(q--){
+    int a,b;
+    cin>>a>>b;
+    a--;b--;
+    if(n==1){
+      cout<<min(a+1,b+1)<<endl;
+      continue;
+    }
+    while(a!=b){
+      while(a>b){
+        a=(a-1)/n;
+      }
+      while(a<b){
+        b=(b-1)/n;
+      }
+    }
+    cout<<a+1<<endl;
+  }
+
+
+
+}
---
+++ atcoder/Other/relay2/relay2_d.cpp
@@ -0,0 +1,126 @@
+
+
+#include<bits/stdc++.h>
+using namespace std;
+#define inf INT_MAX
+#define INF LLONG_MAX
+#define ll long long
+#define ull unsigned long long
+#define M (int)(1e9+7)
+#define P pair<int,int>
+#define FOR(i,m,n) for(int i=(int)m;i<(int)n;i++)
+#define RFOR(i,m,n) for(int i=(int)m;i>=(int)n;i--)
+#define rep(i,n) FOR(i,0,n)
+#define rrep(i,n) RFOR(i,n,0)
+#define all(a) a.begin(),a.end()
+const int vx[4] = {0,1,0,-1};
+const int vy[4] = {1,0,-1,0};
+#define F first
+#define S second
+#define PB push_back
+#define EB emplace_back
+#define int ll
+#define vi vector<int>
+#define IP pair<int,P>
+#define PI pair<P,int>
+#define PP pair<P,P>
+#define Yes(f){cout<<(f?"Yes":"No")<<endl;}
+#define YES(f){cout<<(f?"YES":"NO")<<endl;}
+#define MPlus(x,y) {x=(x+y)%M;}
+#define MSub(x,y) {x=(x-y+M)%M;}
+#define MMul(x,y) {x=(x*y)%M;}
+
+
+
+template< typename T >
+struct edge {
+  int src, to;
+  T cost;
+
+  edge(int to, T cost) : src(-1), to(to), cost(cost) {}
+
+  edge(int src, int to, T cost) : src(src), to(to), cost(cost) {}
+
+  edge &operator=(const int &x) {
+    to = x;
+    return *this;
+  }
+
+  operator int() const { return to; }
+};
+
+template< typename T >
+using Edges = vector< edge< T > >;
+template< typename T >
+using WG = vector< Edges< T > >;
+using UG = vector< vector< int > >;
+template< typename T >
+using Matrix = vector< vector< T > >;
+
+
+
+struct UnionFind
+{
+
+  vector<int> par;
+  vector<int> sizes;
+
+  UnionFind(int n) : par(n), sizes(n, 1) {
+    rep(i,n) par[i] = i;
+  }
+
+  int root(int x) {
+    if (x == par[x]) return x;
+    return par[x] = root(par[x]); 
+  }
+
+  bool unite(int x, int y) {
+
+    x = root(x);
+    y = root(y);
+
+    if (x == y) return false;
+
+    if (sizes[x] < sizes[y]) swap(x, y);
+
+    par[y] = x;
+    sizes[x] += sizes[y];
+
+    return true;
+  }
+
+  bool find(int x,int y){
+    return root(x)==root(y);
+  }
+
+  int size(int x) {
+    return sizes[root(x)];
+  }
+};
+
+signed main(){
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  cout<<fixed<<setprecision(20);
+
+  int n,m;
+  cin>>n>>m;
+  Edges<int> es;
+  UnionFind uf(n);
+  rep(i,m){
+    int a,b;
+    cin>>a>>b;
+    a--;b--;
+    es.PB(edge<int>(a,b,0));
+    uf.unite(a,b);
+  }
+
+  int a=uf.size(0),b=uf.size(1);
+
+  int ans=a*(a-1)/2+(n-a)*(n-a-1)/2-m;
+  ans=max(ans,b*(b-1)/2+(n-b)*(n-b-1)/2-m);
+
+  cout<<ans<<endl;
+
+
+}
