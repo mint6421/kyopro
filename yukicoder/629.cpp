70750a428f20cfc2f15a411ab411e129f5a0cbb8
ee177100 <ee177100@meiji.ac.jp>
2019-11-26 14:50:06 +0900

    2019/11/26

---
+++ yukicoder/629.cpp
@@ -0,0 +1,96 @@
+
+
+#include<bits/stdc++.h>
+using namespace std;
+#define inf INT_MAX
+#define INF LLONG_MAX
+#define ll long long
+#define ull unsigned long long
+#define M (int)(1e9+7)
+#define P pair<int,int>
+#define FOR(i,m,n) for(int i=(int)m;i<(int)n;i++)
+#define RFOR(i,m,n) for(int i=(int)m;i>=(int)n;i--)
+#define rep(i,n) FOR(i,0,n)
+#define rrep(i,n) RFOR(i,n,0)
+#define all(a) a.begin(),a.end()
+const int vx[4] = {0,1,0,-1};
+const int vy[4] = {1,0,-1,0};
+#define F first
+#define S second
+#define PB push_back
+#define EB emplace_back
+#define int ll
+#define vi vector<int>
+#define IP pair<int,P>
+#define PI pair<P,int>
+#define PP pair<P,P>
+#define Yes(f){cout<<(f?"Yes":"No")<<endl;}
+#define YES(f){cout<<(f?"YES":"NO")<<endl;}
+int Mplus(int x,int y) {return (x+y)%M;}
+int Msub(int x,int y) {return (x-y+M)%M;}
+int Mmul(int x,int y) {return (x*y)%M;}
+
+
+
+template< typename T >
+struct edge {
+  int src, to;
+  T cost;
+
+  edge(int to, T cost) : src(-1), to(to), cost(cost) {}
+
+  edge(int src, int to, T cost) : src(src), to(to), cost(cost) {}
+
+  edge &operator=(const int &x) {
+    to = x;
+    return *this;
+  }
+
+  operator int() const { return to; }
+};
+
+template< typename T >
+using Edges = vector< edge< T > >;
+template< typename T >
+using WG = vector< Edges< T > >;
+using UG = vector< vector< int > >;
+template< typename T >
+using Matrix = vector< vector< T > >;
+
+
+signed main(){
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  cout<<fixed<<setprecision(20);
+
+  int n,m;
+  cin>>n>>m;
+  vi a(n);
+  rep(i,n){
+    cin>>a[i];
+  }
+
+  UG es(n);
+  rep(i,m){
+    int x,y;
+    cin>>x>>y;
+    x--;y--;
+    es[x].PB(y);
+    es[y].PB(x);
+  }
+
+  bool flag=false;
+  rep(i,n){
+    int s=a[i];
+    set<int> low,up;
+    for(int x:es[i]){
+      if(a[x]<s) low.insert(a[x]);
+      if(a[x]>s) up.insert(a[x]);
+    }
+    if(low.size()>1||up.size()>1) flag=true;
+  }
+
+  YES(flag);
+
+
+}
