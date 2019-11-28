48481babc4f07198e1129069937fb0db1c30a910
ee177100 <ee177100@meiji.ac.jp>
2019-11-29 00:17:12 +0900

    2019/11/29

---
+++ codeforces/1248/1248C.cpp
@@ -0,0 +1,50 @@
+
+
+#include<bits/stdc++.h>
+using namespace std;
+#define inf INT_MAX
+#define INF LLONG_MAX
+#define ll long long
+#define ull unsigned long long
+#define M (int)(1e9+7)
+#define P pair<int,int>
+#define FOR(i,m,n) for(int i=(int)m;i<(int)n;i++)
+#define RFOR(i,m,n) for(int i=(int)m;i>=(int)n;i--)
+#define rep(i,n) FOR(i,0,n)
+#define rrep(i,n) RFOR(i,n,0)
+#define all(a) a.begin(),a.end()
+const int vx[4] = {0,1,0,-1};
+const int vy[4] = {1,0,-1,0};
+#define F first
+#define S second
+#define PB push_back
+#define EB emplace_back
+#define int ll
+#define vi vector<int>
+#define IP pair<int,P>
+#define PI pair<P,int>
+#define PP pair<P,P>
+#define Yes(f){cout<<(f?"Yes":"No")<<endl;}
+#define YES(f){cout<<(f?"YES":"NO")<<endl;}
+int Madd(int x,int y) {return (x+y)%M;}
+int Msub(int x,int y) {return (x-y+M)%M;}
+int Mmul(int x,int y) {return (x*y)%M;}
+
+
+signed main(){
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  cout<<fixed<<setprecision(20);
+
+  int n,m;
+  cin>>n>>m;
+  int dp[110000];
+  dp[1]=1;
+  FOR(i,2,100100){
+    dp[i]=Madd(dp[i],dp[i-1]);
+    dp[i]=Madd(dp[i],dp[i-2]);
+  }
+
+  cout<<Mmul(2,Msub(Madd(dp[n+1],dp[m+1]),1))<<endl;
+
+}
