d1cc878e2e4dec16057262cfaeadf9b6fdf25298
ee177100 <ee177100@meiji.ac.jp>
2019-12-02 22:04:01 +0900

    2019/12/02

---
+++ yukicoder/657.cpp
@@ -0,0 +1,56 @@
+
+
+#include<bits/stdc++.h>
+using namespace std;
+#define inf INT_MAX
+#define INF LLONG_MAX
+#define ll long long
+#define ull unsigned long long
+#define M 17
+#define P pair<int,int>
+#define FOR(i,m,n) for(int i=(int)m;i<(int)n;i++)
+#define RFOR(i,m,n) for(int i=(int)m;i>=(int)n;i--)
+#define rep(i,n) FOR(i,0,n)
+#define rrep(i,n) RFOR(i,n,0)
+#define all(a) a.begin(),a.end()
+const int vx[4] = {0,1,0,-1};
+const int vy[4] = {1,0,-1,0};
+#define F first
+#define S second
+#define PB push_back
+#define EB emplace_back
+#define int ll
+#define vi vector<int>
+#define IP pair<int,P>
+#define PI pair<P,int>
+#define PP pair<P,P>
+#define Yes(f){cout<<(f?"Yes":"No")<<endl;}
+#define YES(f){cout<<(f?"YES":"NO")<<endl;}
+int Madd(int x,int y) {return (x+y)%M;}
+int Msub(int x,int y) {return (x-y+M)%M;}
+int Mmul(int x,int y) {return (x*y)%M;}
+
+
+signed main(){
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  cout<<fixed<<setprecision(20);
+
+  vi t(1001000);
+  t[0]=0;t[1]=0;t[2]=0;t[3]=1;
+
+  FOR(i,4,1000000){
+    t[i]=t[i-1]+t[i-2]+t[i-3]+t[i-4];
+    t[i]%=M;
+  }
+
+  int q;
+  cin>>q;
+  while(q--){
+    int n;
+    cin>>n;
+    cout<<t[n-1]<<endl;
+  }
+
+
+}
