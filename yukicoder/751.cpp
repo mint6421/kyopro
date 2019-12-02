5fd46c20af035d2a87dcfa7e628910c12873c913
ee177100 <ee177100@meiji.ac.jp>
2019-12-02 22:04:16 +0900

    2019/12/02

---
+++ yukicoder/751.cpp
@@ -0,0 +1,69 @@
+
+
+#include<bits/stdc++.h>
+using namespace std;
+#define inf INT_MAX
+#define INF LLONG_MAX
+#define ll long long
+#define ull unsigned long long
+#define M (int)(1e9+7)
+#define P pair<int,int>
+#define FOR(i,m,n) for(int i=(int)m;i<(int)n;i++)
+#define RFOR(i,m,n) for(int i=(int)m;i>=(int)n;i--)
+#define rep(i,n) FOR(i,0,n)
+#define rrep(i,n) RFOR(i,n,0)
+#define all(a) a.begin(),a.end()
+const int vx[4] = {0,1,0,-1};
+const int vy[4] = {1,0,-1,0};
+#define F first
+#define S second
+#define PB push_back
+#define EB emplace_back
+#define int ll
+#define vi vector<int>
+#define IP pair<int,P>
+#define PI pair<P,int>
+#define PP pair<P,P>
+#define Yes(f){cout<<(f?"Yes":"No")<<endl;}
+#define YES(f){cout<<(f?"YES":"NO")<<endl;}
+int Madd(int x,int y) {return (x+y)%M;}
+int Msub(int x,int y) {return (x-y+M)%M;}
+int Mmul(int x,int y) {return (x*y)%M;}
+
+
+signed main(){
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  cout<<fixed<<setprecision(20);
+
+  int n,m;
+  cin>>n;
+  int s=1,t=1;
+  rep(i,n){
+    int a;
+    cin>>a;
+    if(i) t*=a;
+    else s*=a;
+  }
+
+  cin>>m;
+  rep(i,m){
+    int a;
+    cin>>a;
+    if(i%2) s*=a;
+    else t*=a;
+  }
+
+  //cout<<s<<' '<<t<<endl;
+
+  if(t<0){
+    s*=-1;
+    t*=-1;
+  }
+
+  int u=__gcd(abs(s),abs(t));
+
+  cout<<s/u<<' '<<t/u<<endl;
+
+
+}
