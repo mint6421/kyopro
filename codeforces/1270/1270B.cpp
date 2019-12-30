7dfb2ba58f705ba3cc83c5b87ead423e878fbec8
ee177100 <ee177100@meiji.ac.jp>
2019-12-30 23:21:31 +0900

    2019/12/30

---
+++ codeforces/1270/1270B.cpp
@@ -0,0 +1,62 @@
+
+
+#include<bits/stdc++.h>
+using namespace std;
+#define inf INT_MAX
+#define INF LLONG_MAX
+#define ll long long
+#define ull unsigned long long
+#define M (int)(1e9+7)
+#define P pair<int,int>
+#define FOR(i,m,n) for(int i=(int)m;i<(int)n;i++)
+#define RFOR(i,m,n) for(int i=(int)m;i>=(int)n;i--)
+#define rep(i,n) FOR(i,0,n)
+#define rrep(i,n) RFOR(i,n,0)
+#define all(a) a.begin(),a.end()
+const int vx[4] = {0,1,0,-1};
+const int vy[4] = {1,0,-1,0};
+#define F first
+#define S second
+#define PB push_back
+#define EB emplace_back
+#define int ll
+#define vi vector<int>
+#define IP pair<int,P>
+#define PI pair<P,int>
+#define PP pair<P,P>
+#define Yes(f){cout<<(f?"Yes":"No")<<endl;}
+#define YES(f){cout<<(f?"YES":"NO")<<endl;}
+
+
+signed main(){
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  cout<<fixed<<setprecision(20);
+
+  int q;
+  cin>>q;
+  while(q--){
+    int n;
+    cin>>n;
+    vi a(n);
+    P ans=P(-1,-1);
+    rep(i,n){
+      cin>>a[i];
+      if(i){
+        if(abs(a[i]-a[i-1])>=2){
+          ans=P(i,i+1);
+        }
+      }
+    }
+
+    if(ans.F==-1){
+      cout<<"NO"<<endl;
+    }else{
+      cout<<"YES"<<endl;
+      cout<<ans.F<<' '<<ans.S<<endl;
+    }
+  }
+    
+
+
+}
